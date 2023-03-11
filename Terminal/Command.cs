3ca3cbbb3bb71067071a295674fe6b1f9648b74c
Mindless Sea <averytoad65@gmail.com>
2023-03-11 20:10:45 +0100

    Basic Project

---
+++ Terminal/Command.cs
@@ -0,0 +1,18 @@
+﻿namespace Terminal
+{
+    public class Command
+    {
+        public Command(string name, string desc, CmdFunc func)
+        {
+            this.name = name;
+            this.description = desc;
+            this.function = func;
+        }
+        
+        public string name;
+        public string description;
+
+        public delegate void CmdFunc();
+        public CmdFunc function;
+    }
+}
---
+++ Terminal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Terminal
+{
+    internal class Program
+    {
+        private const string PREFIX = ">";
+        private static Command[] commands = new[]
+        {
+            new Command("help", "Shows this command", Help),
+            new Command("exit", "Quits the command line", Exit)
+        };
+        
+        private static void Help()
+        {
+            foreach (var cmd in commands)
+            {
+                Console.WriteLine($"{cmd.name} - {cmd.description}");
+            }
+        }
+        
+        private static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        public static void Main(string[] _)
+        {
+            while (true)
+            {
+                Console.Write($"{PREFIX} ");
+                string[] args = Console.ReadLine().Split(' ');
+
+                try
+                {
+                    var command = commands.First(x => x.name == args[0].ToLower());
+                    command.function();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{args[0]} not found!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+    }
+}
